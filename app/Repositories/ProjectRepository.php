0d96d9afec6e3ae016831cbb7747507aa775bbbb
jjgesulgon <jj@hybrain.co>
2020-10-18 23:37:03 +0800

    Added rollback in db transaction

--- app/Repositories/ProjectRepository.php
+++ app/Repositories/ProjectRepository.php
@@ -81,28 +81,34 @@ class ProjectRepository extends Repository
    */
     public function update($request, $id)
     {
-        return DB::transaction(function () use ($request, $id) {
-          $project = $this->project->findOrFail($id);
-          $project->fill($request->all());
-          $project->save();
+        try {
+          
+            return DB::transaction(function () use ($request, $id) {
+                $project = $this->project->findOrFail($id);
+                $project->fill($request->all());
+                $project->save();
 
-          $project->techUsed()->delete();
+                $project->techUsed()->delete();
 
-          if ($request->tech_used) {
-                if (sizeof($request->tech_used) > 0) {
-                    for ($ctr = 0; $ctr < count($request->tech_used); $ctr++) {
-                        $techUsed = $project->techUsed()->save(new TechUsed(array('tech_stack_item_id' => array_values($request->tech_used)[$ctr])));
-                        if (! $techUsed) {
-                            return response()->json([
+                if ($request->tech_used) {
+                    if (sizeof($request->tech_used) > 0) {
+                        for ($ctr = 0; $ctr < count($request->tech_used); $ctr++) {
+                            $techUsed = $project->techUsed()->save(new TechUsed(array('tech_stack_item_id' => array_values($request->tech_used)[$ctr])));
+                            if (! $techUsed) {
+                                return response()->json([
                       'message' => 'Failed to store resource'
                   ], 500);
+                            }
                         }
                     }
                 }
-            }
 
-          return $project;
-      });
+                return $project;
+            });
+
+        } catch (\Exception $e) {
+            DB::rollback();
+        }
     }
 
     /**
